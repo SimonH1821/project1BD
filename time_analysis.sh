a2f38bb7eb50a8de0377823f491f087ade38e3d0
SimonH1821 <50848017+SimonH1821@users.noreply.github.com>
2025-03-21 11:18:40 -0400

    Update and rename test.sh to time_analysis.sh

--- time_analysis.sh
+++ time_analysis.sh
@@ -5,8 +5,8 @@
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /project1BD/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/nyc_parking_violations.csv /project1BD/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ../../mapreduce-test-python/project1BD/mapper.py -mapper ../../mapreduce-test-python/project1BD/mapper.py \
--file ../../mapreduce-test-python/project1BD/reducer.py -reducer ../../mapreduce-test-python/project1BD/reducer.py \
+-file ../../mapreduce-test-python/project1BD/mapper.py -mapper ../../mapreduce-test-python/project1BD/mapper_time.py \
+-file ../../mapreduce-test-python/project1BD/reducer.py -reducer ../../mapreduce-test-python/project1BD/reducer_time.py \
 -input /project1BD/input/* -output /project1BD/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /project1BD/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /project1BD/input/
