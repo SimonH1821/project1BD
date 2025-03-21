c9df3e45b5e33f32ff8c8bfd0c0ea26b6eb9eeab
SimonH1821 <50848017+SimonH1821@users.noreply.github.com>
2025-03-21 10:16:00 -0400

    Update mapper_vehicle.py
    Update for correct fields

--- mapper_vehicle.py
+++ mapper_vehicle.py
@@ -4,7 +4,7 @@ import sys
 for line in sys.stdin:
     fields = line.strip().split(',')
 
-    if len(fields) > 4 and fields[2] and fields[5]:  # Ensure columns exist and are not empty
-        vehicle_year = fields[2].strip()
-        vehicle_make = fields[5].strip()
+    if len(fields) > 4 and fields[35] and fields[7]:  # Ensure columns exist and are not empty
+        vehicle_year = fields[35].strip()
+        vehicle_make = fields[7].strip()
         print(f"{vehicle_year},{vehicle_make}\t1")
