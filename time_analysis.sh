9e43aae4043dc63d9978925f08ecf8ed38f3e905
SimonH1821 <50848017+SimonH1821@users.noreply.github.com>
2025-03-22 13:22:02 -0400

    Update time_analysis.sh

--- time_analysis.sh
+++ time_analysis.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 ../../start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /project1BD/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /project1BD/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /project1BD/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/nyc_parking_violations.csv /project1BD/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 -file ../../mapreduce-test-python/project1BD/mapper.py -mapper ../../mapreduce-test-python/project1BD/mapper_time.py \
 -file ../../mapreduce-test-python/project1BD/reducer.py -reducer ../../mapreduce-test-python/project1BD/reducer_time.py \
 -input /project1BD/input/* -output /project1BD/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /project1BD/output/part-00000
-/usr/local/hadoop/bin/hdfs dfs -rm -r /project1BD/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /project1BD/output/
 ../../stop.sh
