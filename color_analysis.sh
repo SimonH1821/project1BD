41b5d11beb8bdea06a668a85ff85ef62b2eb6b6b
SimonH1821 <50848017+SimonH1821@users.noreply.github.com>
2025-03-22 13:21:41 -0400

    Update color_analysis.sh

--- color_analysis.sh
+++ color_analysis.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 ../../start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /project1BD/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /project1BD/output/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 -file ../../mapreduce-test-python/project1BD/mapper_color.py -mapper ../../mapreduce-test-python/project1BD/mapper_color.py \
