137a69a378ad3f65928672825fab91afe69a063b
SimonH1821 <50848017+SimonH1821@users.noreply.github.com>
2025-03-22 13:22:22 -0400

    Update vehicle_analysis.sh

--- vehicle_analysis.sh
+++ vehicle_analysis.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 ../../start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /project1BD/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /project1BD/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /project1BD/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/nyc_parking_violations.csv /project1BD/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 -file ../../mapreduce-test-python/project1BD/mapper_vehicle.py -mapper ../../mapreduce-test-python/project1BD/mapper_vehicle.py \
 -file ../../mapreduce-test-python/project1BD/reducer_vehicle.py -reducer ../../mapreduce-test-python/project1BD/reducer_vehicle.py \
 -input /project1BD/input/* -output /project1BD/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /project1BD/output/part-00000
-/usr/local/hadoop/bin/hdfs dfs -rm -r /project1BD/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /project1BD/output/
 ../../stop.sh
