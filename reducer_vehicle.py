3f01ebd88d3f94462e7de85e9c80261160468fca
SimonH1821 <50848017+SimonH1821@users.noreply.github.com>
2025-03-24 18:41:48 -0400

    Update reducer_vehicle.py

--- reducer_vehicle.py
+++ reducer_vehicle.py
@@ -15,7 +15,7 @@ for line in sys.stdin:
         continue  # Skip invalid lines
 
 # Sort by count in descending order
-sorted_vehicles = sorted(vehicle_counts.items(), key=lambda x: x[1], reverse=True)
+sorted_vehicles = sorted(vehicle_counts.items(), key=lambda x: x[1], reverse=True)[:100] # added to limit to the top 100 or you will not see the top values in the terminal
 
 # Print sorted results
 for vehicle, count in sorted_vehicles:
